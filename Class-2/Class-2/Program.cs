093cb2ac157728ba49c8feedc78fa1b73bcab716
Mohammad.Tomaraei <themreza@gmail.com>
2019-02-27 11:26:23 +0100

    Class 2

---
+++ Class-2/Class-2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_2
+{
+    class Program
+    {
+        // The "ref" keyword will use and modify the original variable passed to the function.
+        // Not using it will create a copy of the input arguments in the memory.
+        static void ReadVar(ref int x, ref int y)
+        {
+            Console.Write("A=");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("B=");
+            y = int.Parse(Console.ReadLine());
+        }
+
+        static void ReadVar2(string s, ref int x)
+        {
+            Console.Write(s);
+            x = int.Parse(Console.ReadLine());
+        }
+
+        static int ReadVar3(string s)
+        {
+            int a; // Local variable a
+            Console.Write(s);
+            a = int.Parse(Console.ReadLine());
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c;
+            //ReadVar(ref a, ref b);
+            //ReadVar2("A=", ref a);
+            //ReadVar2("B=", ref b);
+            a = ReadVar3("A=");
+            b = ReadVar3("B=");
+            c = a + b;
+            Console.WriteLine("A+B={0}", c);
+      
+            Console.ReadLine();
+        }
+    }
+}
